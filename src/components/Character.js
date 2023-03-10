4461b8b9d3137e099ff4fc0151290451e6017599
MarilynWho <viktoriia.cheche@gmail.com>
2023-03-10 19:08:40 +0000

    Fix img src

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 const Character = (props) => {
-  const { name, imgURL, birth, death, race, realm, spouse } = props;
+  const { name, imgUrl, birth, death, race, realm, spouse } = props;
   return (
     <div data-test="component-character">
       <h1 data-test="char-name">{name}</h1>
-      <img data-test="char-img" src={imgURL} alt={name} />
+      <img data-test="char-img" src={imgUrl} alt={name} />
       <ul data-test="char-list">
         <li data-test="char-birth">Date of Birth: {birth}</li>
         <li data-test="char-death">Date of Death: {death}</li>
