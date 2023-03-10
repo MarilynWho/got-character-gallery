859d3583a537c28fd12434d0bec72b17c0eb30c2
MarilynWho <viktoriia.cheche@gmail.com>
2023-03-10 18:58:22 +0000

    YAML file to automate workflow

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import CharacterGallery from "./components/CharacterGallery";
-import characters from "./data/characterData.json";
 
 function App() {
   return (
